0d8c8e741864c251bab78011f6cc8acc813c2c86
Minyee <Minyee@atguigu.com>
2022-04-18 23:11:51 +0800

    first commit

---
+++ src/main/java/com/atguitu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguitu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-04-18 23:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
